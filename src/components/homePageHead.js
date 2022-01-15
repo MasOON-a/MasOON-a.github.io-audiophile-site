bef446b419c065b9c9002dc57b3151b690a02aa0
MasOON-a <masonmatich@gmail.com>
2022-01-15 15:44:56 -0800

    commit to main

--- src/components/homePageHead.js
+++ src/components/homePageHead.js
@@ -5,7 +5,7 @@ return(
     <div className="home-page-head">
         <div className="info-cont">
             <h4>new product</h4>
-            <h2>xx99 mark ii heapdhones</h2>
+            <h2>xx99 mark ii headphones</h2>
             <p>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
             <Link className="btn1" to='/headphones/xx99mark2' exact="true">see product</Link>
         </div>
